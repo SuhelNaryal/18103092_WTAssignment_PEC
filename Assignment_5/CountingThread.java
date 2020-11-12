61aba6f1ab34e26cd985ccb3c9b56e1b28514dc9
SuhelNaryal <54847737+SuhelNaryal@users.noreply.github.com>
2020-11-12 21:12:43 +0530

    Question 1 answer

---
+++ Assignment_5/CountingThread.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+public class CountingThread extends Thread
+{
+	public void run()
+    {
+        for(int i=1;i<=100;i++)
+        {
+            System.out.println(i);
+            try
+            {
+                Thread.sleep(1000);
+                if(i%10==0)
+                {
+                	System.out.println("String printed after every ten number counting.");
+                }
+            }
+            catch(Exception e)
+            {
+            	System.out.println(e);
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        CountingThread counting = new CountingThread();
+        counting.start();
+    }
+}
